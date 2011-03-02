258837db4c240d4999adb51e15798679aca644bc
Whitie <weimann.th@gmail.com>
2011-03-02 08:16:17 +0100

    Added index view for orders app.

--- ozone/orders/views.py
+++ ozone/orders/views.py
@@ -1,7 +1,26 @@
 # Create your views here.
 
+from datetime import date
+
+from django.template import RequestContext
+from django.shortcuts import render_to_response, redirect
+from django.core.paginator import Paginator, InvalidPage, EmptyPage
+from django.utils.translation import ugettext_lazy as _
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required, permission_required
+
 from orders.forms import OrderDayForm
+from orders.models import OrderDay, Order
+from orders.menu import menus
 
 
 def index(req):
-    pass
+    if req.user.has_perm('orders.can_order'):
+        q = dict(day__gte=date.today())
+    else:
+        q = dict(day__gt=date.today())
+    odays = OrderDay.objects.select_related().filter(**q)
+    ctx = dict(page_title=_(u'Orders'), odays=odays, menus=menus)
+    return render_to_response('orders/index.html', ctx,
+                              context_instance=RequestContext(req))
+
