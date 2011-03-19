6499bfc2df18ac803b5b783fba46431710f16924
Whitie <weimann.th@gmail.com>
2011-03-19 08:30:56 +0100

    Added box to display logged in user.

--- ozone/core/context_processors.py
+++ ozone/core/context_processors.py
@@ -5,8 +5,8 @@ def userconf(req):
     ctx = {'_profile': None, '_config': {}}
     if req.user.is_authenticated():
         p = req.user.get_profile()
-        ctx['_profile'] = p
-        ctx['_config'] = p.config()
+        ctx['userprofile'] = p
+        ctx['userconfig'] = p.config()
     return ctx
 
 def set_global_vars(req):
