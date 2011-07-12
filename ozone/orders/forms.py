973d691b613a113686427a542b63aa73293b2596
Whitie <weimann.th@gmail.com>
2011-07-12 08:06:37 +0200

    Changed orders.forms to HTML 5 widgets.

--- ozone/orders/forms.py
+++ ozone/orders/forms.py
@@ -4,6 +4,8 @@ from django import forms
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 
+import core.html5_widgets as widgets
+
 
 USER_CHOICES = [(x.id, x.get_full_name() or x.username) for x in
                 User.objects.all() if x.has_perm('orders.add_orderday')]
@@ -11,6 +13,6 @@ DATE_FORMATS = ['%d.%m.%Y', '%d.%m.%y', '%d/%m/%Y', '%d/%m/%y']
 
 class OrderDayForm(forms.Form):
     day = forms.DateField(label=_(u'Next Order Day'),
-                          input_formats=DATE_FORMATS)
-    user = forms.ChoiceField(label=_(u'Responsible User'), choices=USER_CHOICES,
-                             help_text=_(u'Use this format: dd.mm.yyyy'))
+        input_formats=DATE_FORMATS, widget=widgets.DateInput5(),
+        help_text=_(u'Use this format: dd.mm.yyyy'))
+    user = forms.ChoiceField(label=_(u'Responsible User'), choices=USER_CHOICES)
