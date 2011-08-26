9fdf12a1cea448569c0f94c73eab9412d8a3bd5e
Whitie <weimann.th@gmail.com>
2011-08-26 08:42:51 +0200

    Fixed bug with new URL names in orders/menu.py

--- ozone/orders/menu.py
+++ ozone/orders/menu.py
@@ -6,7 +6,7 @@ from core.menu import Menu
 
 
 order_menu = Menu(_(u'Orders'),
-    ('orders-old', _(u'Order now')),
+    ('orders-ask', _(u'Order now')),
     ('orders-myorders', _(u'My Orders')),
 )
 
