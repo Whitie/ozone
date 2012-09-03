db7f6e0734cd67b9f83ac30b9bbe4b825be42e07
Whitie <weimann.th@gmail.com>
2012-09-03 20:43:20 +0200

    Added path to sys.path in wsgi.py.

--- ozone/wsgi.py
+++ ozone/wsgi.py
@@ -14,8 +14,11 @@ framework.
 
 """
 import os
+import sys
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ozone.settings")
+PATH = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
+sys.path.insert(0, PATH)
 
 # This application object is used by any WSGI server configured to use this
 # file. This includes Django's development server, if the WSGI_APPLICATION
