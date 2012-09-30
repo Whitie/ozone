0a103c49ee4ef06a64848fd80471dcf1e9feb9b0
Whitie <weimann.th@gmail.com>
2012-09-30 09:05:59 +0200

    Added edit page for company rating management.

--- orders/views/helper.py
+++ orders/views/helper.py
@@ -98,11 +98,14 @@ def calculate_ratings(companies):
             whole_ratings[r.rating] += 1
             collected.extend(r.as_list())
             if r.note:
-                notes.append(r.note)
+                notes.append(u'%s: %s' % (r.user.last_name, r.note))
         sratings = sorted(whole_ratings.items(), key=lambda x: x[1])
         tmp = [u'%dx %s' % (y, x) for x, y in sratings]
         c.whole_ratings = u', '.join(tmp)
-        c.min_rating = min(collected)
+        try:
+            c.min_rating = min(collected)
+        except ValueError:
+            c.min_rating = u'-'
         c.calculated, c.average = c.calculate_rating()
         c.raterlist = [x.last_name for x in c.rating_users.all()]
         c.rating_notes = notes
