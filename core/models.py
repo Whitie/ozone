5e29656199adca8f4a126a0367595947af6762b5
Whitie <weimann.th@gmail.com>
2012-12-03 11:43:08 +0100

    Changed value to initial.

--- core/models.py
+++ core/models.py
@@ -6,7 +6,6 @@ from datetime import date
 from mimetypes import guess_type
 
 from django.db import models
-from django.conf import settings
 from django.contrib.auth.models import User
 from django.utils.translation import ugettext_lazy as _
 from django.utils.safestring import mark_safe
