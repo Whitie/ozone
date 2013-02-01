1eadf15d762c24d6741e039b8bf1d62238388a3e
Whitie <weimann.th@gmail.com>
2013-02-01 09:44:18 +0100

    Integrated admin branding with bootstrap theme.

--- ozone/settings.py
+++ ozone/settings.py
@@ -141,7 +141,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     # Uncomment the next line to enable the admin:
-    'django_admin_bootstrapped',
+    'bootstrap_admin',
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     'django.contrib.admindocs',
