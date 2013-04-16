f065922b23bc35c2e947abe28c0d25eb1001beca
Whitie <weimann.th@gmail.com>
2013-04-16 09:14:30 +0200

    Added secure_filename function from werkzeug project.

--- core/utils.py
+++ core/utils.py
@@ -1,5 +1,7 @@
 # -*- coding: utf-8 -*-
 
+import os
+import re
 import traceback
 
 from datetime import date, datetime
@@ -13,6 +15,57 @@ from django.utils.translation import ugettext_lazy as _
 from django.contrib.auth.decorators import user_passes_test
 from django.core.exceptions import PermissionDenied
 from django.contrib.sessions.models import Session
+
+
+# Taken from werkzeug <http://werkzeug.pocoo.org>
+_filename_ascii_strip_re = re.compile(r'[^A-Za-z0-9_.-]')
+_windows_device_files = ('CON', 'AUX', 'COM1', 'COM2', 'COM3', 'COM4', 'LPT1',
+                         'LPT2', 'LPT3', 'PRN', 'NUL')
+
+
+def secure_filename(filename):
+    r"""Pass it a filename and it will return a secure version of it. This
+    filename can then safely be stored on a regular file system and passed
+    to :func:`os.path.join`. The filename returned is an ASCII only string
+    for maximum portability.
+
+    On windows system the function also makes sure that the file is not
+    named after one of the special device files.
+
+    >>> secure_filename("My cool movie.mov")
+    'My_cool_movie.mov'
+    >>> secure_filename("../../../etc/passwd")
+    'etc_passwd'
+    >>> secure_filename(u'i contain cool \xfcml\xe4uts.txt')
+    'i_contain_cool_umlauts.txt'
+
+    The function might return an empty filename. It's your responsibility
+    to ensure that the filename is unique and that you generate random
+    filename if the function returned an empty one.
+
+    .. versionadded:: 0.5
+
+    :param filename: the filename to secure
+    """
+    if isinstance(filename, unicode):
+        from unicodedata import normalize
+        filename = normalize('NFKD', filename).encode('ascii', 'ignore')
+    for sep in os.path.sep, os.path.altsep:
+        if sep:
+            filename = filename.replace(sep, ' ')
+    filename = str(_filename_ascii_strip_re.sub('', '_'.join(
+                   filename.split()))).strip('._')
+
+    # on nt a couple of special files are present in each folder. We
+    # have to ensure that the target file is not such a filename. In
+    # this case we prepend an underline
+    if os.name == 'nt' and filename and \
+       filename.split('.')[0].upper() in _windows_device_files:
+        filename = '_' + filename
+
+    return filename
+
+# End of werkzeug code #######################################################
 
 
 def named(verbose_name):
