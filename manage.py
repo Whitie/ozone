0c42fc03765a8ccb686d1a739e40b0ca53b574a1
Whitie <weimann.th@gmail.com>
2013-06-17 08:34:52 +0200

    Fixed path.

--- manage.py
+++ manage.py
@@ -5,7 +5,8 @@ import sys
 
 # For testing
 _PATH = os.path.dirname(os.path.abspath(__file__))
-sys.path.insert(0, os.path.join(_PATH, '..', 'ozone_extensions'))
+_EXT_PATH = os.path.join(_PATH, '..', 'ozone_extensions')
+sys.path.insert(0, os.path.normpath(_EXT_PATH))
 
 
 if __name__ == "__main__":
