4879bb59ef4cba13615efcfaaa15ddf152699404
Whitie <weimann.th@yahoo.com>
2020-01-14 20:51:48 +0100

    PEP 8

--- ozone/urls.py
+++ ozone/urls.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-from django.conf.urls import *
+from django.conf.urls import patterns, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.views.generic import RedirectView
@@ -11,19 +11,21 @@ admin.autodiscover()
 
 handler500 = 'core.utils.internal_server_error'
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     (r'^$', 'core.views.web.index'),
     (r'^core/', include('core.urls')),
     (r'^orders/', include('orders.urls')),
-#    (r'^chemdb/', include('chemdb.urls')),
-    (r'^desktop/', include('desktop.urls')),
+    # (r'^chemdb/', include('chemdb.urls')),
+    # (r'^desktop/', include('desktop.urls')),
 
     (r'^admin/', include(admin.site.urls)),
     (r'^external_auth/$', 'core.views.ext_auth.external_auth'),
 )
 
 if settings.DEBUG:
-    urlpatterns += patterns('',
+    urlpatterns += patterns(
+        '',
         (r'^favicon\.ico$', RedirectView.as_view(
             url='/static/img/favicon.ico')),
     ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
