4290e4b749d65a1a5d632652799d2ccaeb6af14a
Thorsten Weimann <weimann.th@yahoo.com>
2020-01-14 09:09:43 +0100

    Added migration.

---
+++ orders/migrations/0008_auto_20200114_0841.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+import datetime
+import django.core.validators
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('orders', '0007_auto_20200108_1328'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='orderday',
+            name='day',
+            field=models.DateField(unique=True, verbose_name='Day', validators=[django.core.validators.MinValueValidator(datetime.date(2020, 1, 14))]),
+            preserve_default=True,
+        ),
+    ]
